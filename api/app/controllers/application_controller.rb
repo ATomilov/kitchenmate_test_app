bef4d876d7e9d1e4534d617e912a84383270c99e
Alexey Tomilov <apofeosz@yandex.ru>
2021-11-21 23:03:02 +0600

    Updated the ::ApplicationController

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  private
+
+  def render_json_response(result:, status: 400)
+    if result.success?
+      render json: result.value!
+    else
+      render json: result.failure, status: status
+    end
+  end
 end
