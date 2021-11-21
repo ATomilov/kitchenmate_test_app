a1303ba5a636f5e98636078cb1d730a88c1784e0
Alexey Tomilov <apofeosz@yandex.ru>
2021-11-21 23:19:48 +0600

    Fixed CI

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_145159) do
-
+ActiveRecord::Schema.define(version: 20_211_121_145_159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,5 +27,4 @@ ActiveRecord::Schema.define(version: 2021_11_21_145159) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
